03a921e7179e4768596bef5e5bd00258dcf1931c
dip4k <dip4k.chauhan@outlook.com>
2018-07-11 17:56:26 +0530

    add : error-handling and toast notification to project

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routes';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
+import { ErrorLogService } from './error-handling/error.log.service';
+import { AppErrorHandler } from './error-handling/app.error.handler';
 
 @NgModule({
   declarations: [
@@ -22,13 +26,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    AppRoutingModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [ErrorLogService, { provide: ErrorHandler, useClass: AppErrorHandler }],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- ClientApp/src/app/components/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: []
 })
 export class NavMenuComponent {
   isExpanded = false;
